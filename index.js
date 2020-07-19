7c0cf30f6c1a1c2ed5f3f28a1da5d48b458f4589
whatsaaaa <aleksandarmilicevic18@gmail.com>
2020-07-19 16:48:52 +0200

    GetMiddlePoint between two coordinates

--- index.js
+++ index.js
@@ -64,12 +64,26 @@ const overrideDefaultOptions = (options) => {
 
 const createIndicator = (coordinates, leafletMap) => {
   for (let i = 1; i < coordinates.length; i++) {
-    let icon = createLeafletDivIcon(coordinate[i]);
+    let icon = createLeafletDivIcon(coordinates[i]);
 
     for (let n = 1; n <= DEFAULT_OPTIONS.numberOfIndicators; n++) {
-      
+      result.push(
+        window.L.marker(
+          getMiddlePoint(
+            coordinates[i],
+            coordinates[i - 1],
+            n / (DEFAULT_OPTIONS.numberOfIndicators + 1),
+            leafletMap
+          ),
+          {
+            icon: icon,
+          }
+        )
+      );
     }
   }
+
+  return result;
 };
 
 const createLeafletDivIcon = (coordinate) => {
@@ -77,16 +91,56 @@ const createLeafletDivIcon = (coordinate) => {
     className: "direction-indicator",
     bgPos: [5, 5],
     html: `
-      <div style="color: ${DEFAULT_OPTIONS.color}; transform: rotate(${calculateIndicatorAngle(coordinates[i -1], coordinates[i], -1)});">▶</div>
+      <div style="color: ${
+        DEFAULT_OPTIONS.color
+      }; transform: rotate(${calculateIndicatorAngle(
+      coordinates[i - 1],
+      coordinates[i],
+      -1
+    )});">▶</div>
     `,
   });
 };
 
-const calculateIndicatorAngle = (firstCoordinate, secondCoordinate, coef) {
+const calculateIndicatorAngle = (firstCoordinate, secondCoordinate, coef) => {
   let dy = secondCoordinate[0] - firstCoordinate[0];
   let dx = secondCoordinate[1] - firstCoordinate[1];
   let theta = Math.cos((Math.PI / 180) * firstCoordinate[0]) * dx;
   let angle = (Math.atan2(dy, theta) / Math.PI) * 180 * coef;
 
   return angle.toFixed(2);
+};
+
+const getMiddlePoint = (
+  firstCoordinate,
+  secondCoordinate,
+  howMany,
+  leafletMap
+) => {
+  let x = leafletMap.project(new window.L.latLng(firstCoordinate));
+  let y = leafletMap.project(new window.L.latLng(secondCoordinate));
+  let halfDistance = getDistance(x, y) * howMany;
+  let distance = getDistance(x, y);
+
+  if (halfDistance === 0) return leafletMap.unproject(x);
+
+  if (distance > halfDistance) {
+    let ratio = (distance - halfDistance) / distance;
+    let result = leafletMap.unproject(
+      new Point(x.x - ratio * (y.x - x.x), y.y - ratio * (y.y - x.y))
+    );
+    return [result.lat, result.lng];
+  }
+};
+
+const getDistance = (pointOne, pointTwo) => {
+  let x = pointTwo.x - pointOne.x;
+  let y = pointTwo.y - pointOne.y;
+
+  return Math.sqrt(x * x + y * y);
+};
+
+function Point(x, y, round) {
+  this.x = round ? Math.round(x) : x;
+  this.y = round ? Math.round(y) : y;
 }
