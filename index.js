19aa93085f0b7f8bcd439304d953b1b56dc6c7c9
whatsaaaa <aleksandarmilicevic18@gmail.com>
2020-07-19 16:37:42 +0200

    Calculate angle between two coordinates and create leaflet icon

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ let DEFAULT_OPTIONS = {
   color: "blue",
 };
 
+let result = [];
+
 const createDirectionIndicators = (coordinates, leafletMap, options) => {
   try {
     validateParameters(coordinates, leafletMap, options);
     overrideDefaultOptions(options);
+    createIndicator(coordinates, leafletMap);
   } catch (e) {
     console.error("[x] Validation failed. Error: ", e);
     return [];
@@ -58,3 +61,32 @@ const overrideDefaultOptions = (options) => {
     DEFAULT_OPTIONS.color = options.color;
   }
 };
+
+const createIndicator = (coordinates, leafletMap) => {
+  for (let i = 1; i < coordinates.length; i++) {
+    let icon = createLeafletDivIcon(coordinate[i]);
+
+    for (let n = 1; n <= DEFAULT_OPTIONS.numberOfIndicators; n++) {
+      
+    }
+  }
+};
+
+const createLeafletDivIcon = (coordinate) => {
+  return window.L.divIcon({
+    className: "direction-indicator",
+    bgPos: [5, 5],
+    html: `
+      <div style="color: ${DEFAULT_OPTIONS.color}; transform: rotate(${calculateIndicatorAngle(coordinates[i -1], coordinates[i], -1)});">▶</div>
+    `,
+  });
+};
+
+const calculateIndicatorAngle = (firstCoordinate, secondCoordinate, coef) {
+  let dy = secondCoordinate[0] - firstCoordinate[0];
+  let dx = secondCoordinate[1] - firstCoordinate[1];
+  let theta = Math.cos((Math.PI / 180) * firstCoordinate[0]) * dx;
+  let angle = (Math.atan2(dy, theta) / Math.PI) * 180 * coef;
+
+  return angle.toFixed(2);
+}
